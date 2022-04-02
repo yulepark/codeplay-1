7dcdf24db12ba0e1d2ee4834c7c78964fbfeb38a
songhyunsoo85 <codeplay.hyunsoo@gmail.com>
2022-04-02 14:52:04 +0900

    파이게임설치완료

--- pygame/pygame_test.py
+++ pygame/pygame_test.py
@@ -1,13 +1,13 @@
 # -*- coding utf-8 -*_
 import pygame
 
-pygmae.init()
+pygame.init()
 screen  = pygame.display.set_mode((600, 600))
-clock = pygame.time.clock
+clock = pygame.time.Clock
 
 while True:
     screen.fill((0, 0 ,0))
-ㅓㅇ
+
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         break
