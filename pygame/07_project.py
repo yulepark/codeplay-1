4c61e423196bd8ca40323eab540116233530d3b0
songhyunsoo85 <codeplay.hyunsoo@gmail.com>
2022-05-14 14:30:49 +0900

    똥피하기 세로버전.

--- pygame/07_project.py
+++ pygame/07_project.py
@@ -1,11 +1,19 @@
 #-*- coding: utf-8 -*-
-from turtle import width
+import random
 import pygame
 pygame.init()
 
 
-screen_width = 480
-screen_height = 640
+game_font = pygame.font.Font(None, 40)
+
+total_time = 0
+
+start_ticks = pygame.time.get_ticks()
+
+
+enemy_speed = 10
+screen_width = 640
+screen_height = 480
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 
@@ -21,18 +29,16 @@ character = pygame.image.load("pygame/source/character.png")
 character_size = character.get_rect(). size
 character_width = character_size[0]
 character_height = character_size[1]
-character_xPos = (screen_width / 2) - (character_width / 2)
-character_yPos = screen_height - character_height
+character_xPos = 0
+character_yPos = screen_height / 2 - (character_height / 2)
 
+enemy = pygame.image.load
 enemy = pygame.image.load("pygame/source/enemy.png")
 enemy_size = enemy.get_rect().size
 enemy_width = enemy_size[0]
 enemy_height = enemy_size[1]
-enemy_xPos = screen_width / 2 - enemy_width
-enemy_yPos = screen_height / 2 - enemy_height
-
-
-
+enemy_xPos = 640 - enemy_width
+enemy_yPos = screen_height / 2 - (enemy_height / 2)
 to_x = 0
 to_y = 0
 
@@ -40,42 +46,34 @@ to_y = 0
 running = True
 while running:
     dt = clock.tick(60)
-    # print("fps : " + str(clock.get_fps()))
+    #print("fps : " + str(clock.get_fps()))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                to_x -= character_speed
-            elif event.key == pygame.K_RIGHT:
-                to_x += character_speed
-            elif event.key == pygame.K_UP:
+            if event.key == pygame.K_UP:
                 to_y -= character_speed
             elif event.key == pygame.K_DOWN:
                 to_y += character_speed
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                to_x = 0
-            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 to_y = 0
 
-    character_xPos += to_x * dt
+    
     character_yPos += to_y * dt
 
-    if character_xPos < 0:
-        character_xPos = 0
-    elif character_xPos > screen_width - character_width:
-        character_xPos = screen_width - character_width
     
     if character_yPos < 0:
         character_yPos = 0
     elif character_yPos > screen_height - character_height:
         character_yPos = screen_height - character_height
-            
+
+
+
 
     
     character_rect = character.get_rect()
-    character_rect.left = character_xPos
+    character_rect.left = character_xPos   
     character_rect.top = character_yPos
 
     enemy_rect = enemy.get_rect()
@@ -83,9 +81,20 @@ while running:
     enemy_rect.top = enemy_yPos
 
 
+    enemy_xPos -= enemy_speed
+    if enemy_xPos < 0:
+        enemy_xPos = screen_width - enemy_width
+        enemy_yPos = random.randint(0, (screen_width - enemy_width))
+        enemy_speed = random.randint(5, 15)
+
+
     if character_rect.colliderect(enemy_rect):
         print(" ^ 3 ^ ")
+        print("당신의 점슈는 {0}점 입니다.".format(total_time))
         running = False
+
+    elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000
+    timer = game_font.render(str(int(total_time + elapsed_time)), False, (0, 0, 0))
 
 
 
@@ -93,9 +102,13 @@ while running:
     screen.blit(bg, (0, 0))
     screen.blit(character, (character_xPos, character_yPos))
     screen.blit(enemy, (enemy_xPos, enemy_yPos))
+    screen.blit(timer , (10, 10))
     pygame.display.update()
 
 
-pygame.quit()
 
+
+
+
+pygame.quit()
 
