40900ad11e5c051b1a83cdf298ae166ab905237f
songhyunsoo85 <codeplay.hyunsoo@gmail.com>
2022-07-23 14:21:11 +0900

    클래스 마지막

---
+++ python_class/character_class.py
@@ -0,0 +1,44 @@
+class 캐릭터():
+    def __init__ (self, 체력, 공격력, 이속):
+        self.체력 = 체력 
+        self.공격력 = 공격력
+        self.이속 = 이속
+
+    def 상태보기(name):
+        print(name)
+        print(self.체력)
+
+    def 상태보기(self, name):
+        print(name)
+        print("체력 : {}".format(self.체력))
+        print("공격력 : {}".format(self.공격력))
+        print("이속 : {}".format(self.이속)    
+        
+class 챔피언(캐릭터):
+    def __init__(self, 체력, 공격력, 이속, q, w, e, r): #야스오가 가져야 할 값들
+        super().__init__(체력, 공격력, 이속) #상속받기 전에 있던 것들중 가져와야 할것들.
+        self.q = q #새로 추가해야할 값들
+        self.w = w
+        self.e = e
+        self.r = r
+
+        def 상태보기(self, name):
+            super.상태보기(name)
+            print("q스킬 : {}".format(self.q))
+            print("w스킬 : {}".format(self.w))
+            print("e스킬 : {}".format(self.e)
+            print("r스킬 : {}".format(self.r))
+            
+
+
+    
+    
+    미니언01 = 캐릭터(100, 5, 20)
+    미니언01.상태보기("미니언1번의 상태")
+    야스오 = 챔피언(100, 1000, 500, "찌르기", "장막", "돌진", "난도질")
+    야스오.상태보기("야스오의 상태")
+    
+    q = 찌르기
+    W = 장막
+    e = 돌진
+    r = 난도질
